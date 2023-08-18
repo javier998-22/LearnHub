9408f03843b63cf90d50f875a938c65254c7196b
Afro_funk <javierygnacio619@gmail.com>
2023-08-18 16:26:59 -0400

    Registro

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut} from '@angular/fire/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor( 
+    private afAuth: Auth
+  ) { }
+  
+  async register(email: string, password: string){
+    const user = await createUserWithEmailAndPassword(this.afAuth, email, password);
+    return await signInWithEmailAndPassword(this.afAuth, email, password);
+  }
+
+  login(email: string, password: string){
+    return signInWithEmailAndPassword(this.afAuth, email, password);
+  }
+
+  logout(){
+    return signOut(this.afAuth);
+  }
+
+}
--- src/app/sign-up/sign-up.page.ts
+++ src/app/sign-up/sign-up.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../services/auth.service';
+import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +10,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpPage implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService, public toastController: ToastController, private router: Router) { }
 
+  newUserFields = {
+    Name: '',
+    Surname: '',
+    Email: '',
+    Password: '',
+    ConfirmPassword: '',
+    TypeUser: ''
+  }
+  Validmessage = 0
   ngOnInit() {
+  }
+
+  async CreateUser(){
+    if (this.newUserFields.Name == "") {
+      this.presentToast("El campo de nombre esta incompleto");
+    } else if (this.newUserFields.Surname == "") {
+      this.presentToast("El campo de apellido esta incompleto");
+    } else if (this.newUserFields.Email == "") {
+      this.presentToast("El campo de email esta incompleto");
+    } else if (this.newUserFields.Password == "") {
+      this.presentToast("El campo de contraseña esta incompleto");
+    } else if (this.newUserFields.ConfirmPassword == "") {
+      this.presentToast("El campo de repetir contraseña esta incompleto");
+    } else if (this.newUserFields.Name == "" || this.newUserFields.Surname == "" || this.newUserFields.Email == "" || this.newUserFields.Password == "" || this.newUserFields.ConfirmPassword == "") {
+    this.Validmessage = 1
+    }
+    else if (this.newUserFields.Password != this.newUserFields.ConfirmPassword) {
+      this.Validmessage = 2
+  }
+  else {
+    this.Validmessage = 0
+    await this.auth.register(this.newUserFields.Email, this.newUserFields.Password);
+    this.router.navigate(['/login'])
+  }
+  console.log(this.newUserFields.TypeUser);
+  }
+  async presentToast(message: string, duration?: number) {
+    const toast = await this.toastController.create(
+      {
+        message: message,
+        duration: duration ? duration : 2000
+      }
+    );
+    toast.present();
   }
 
 }
