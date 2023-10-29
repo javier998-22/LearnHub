6b2395f696c4c2d3e147755de3c7e7611b2fd879
jonathan822 <80842902+jonathan822@users.noreply.github.com>
2023-10-29 18:48:34 -0300

    Perfiladmin ready

--- src/app/Pages/perfil-admin/perfil-admin.page.ts
+++ src/app/Pages/perfil-admin/perfil-admin.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataServiceService } from '../../services/data-service.service';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-perfil-admin',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil-admin.page.scss'],
 })
 export class PerfilAdminPage implements OnInit {
+  id: any;
 
-  constructor() { }
+  uid: any;
+  constructor(private dtS: DataServiceService, private atS: AuthService,  private router: Router) {
+    this.dtS.getUsuarios(this.atS.getUid()).subscribe(res => {
+      if(res){
+        this.uid = res;
+        this.id = this.atS.getUid();
+        this.uid.email = this.atS.getEmail();
+      }
+    });
+  }
 
   ngOnInit() {
   }
 
+  logout(){
+    this.atS.logout();
+    this.router.navigate(['/welcome']);
+  }
+  IraPanel(){
+    this.router.navigate(['/panel']);
+  }
+  IraMod(){
+    this.router.navigate(['/mod-perfil']);
+  }
+
+  
 }
+
