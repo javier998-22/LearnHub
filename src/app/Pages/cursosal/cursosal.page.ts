2341e1720d6e75a206aeeee3754a70505fc1d083
Afro_funk <javierygnacio619@gmail.com>
2023-11-24 14:22:13 -0300

    sisi

--- src/app/Pages/cursosal/cursosal.page.ts
+++ src/app/Pages/cursosal/cursosal.page.ts
@@ -4,6 +4,7 @@ import { DataServiceService } from '../../services/data-service.service';
 import { ModalController } from '@ionic/angular';
 import { MisnotasModalPage } from '../misnotas-modal/misnotas-modal.page';
 import { VerevPage } from '../verev/verev.page';
+import { ChatPage } from '../chat/chat.page';
 
 @Component({
   selector: 'app-cursosal',
@@ -42,6 +43,16 @@ export class CursosalPage implements OnInit {
     await modal.present();
   }
 
+  async abrirchat(Value:any) {
+    const modal = await this.modalController.create({
+      component: ChatPage,
+      cssClass: 'myclass',
+      componentProps:{
+        modalInfo: Value
+      }
+    });
+    await modal.present();
+  }
   async volver(){
     this.router.navigate(['/cursos-a']);
     await this.modalCtrl.dismiss();
